831bb3b2047a4431ed4e70321110154626be8abc
apple <vijay.tambe@questionpro.com>
2019-03-25 14:59:56 +0530

    push

---
+++ src/com/company/Main.java
@@ -0,0 +1,54 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static String command;
+    public static void main(String[] args) {
+
+        do {
+
+            if (commandIsNotNullOREmpty(command == null, command == "")) {
+                printInro();
+            }
+
+            takeCommand();
+
+
+            if (commandIsNotNullOREmpty(command != null, !command.equals(""))) {
+                if (checkAndExecuteCommand()) break;
+            }
+
+        } while (true);
+    }
+
+    private static boolean commandIsNotNullOREmpty(boolean b, boolean b2) {
+        return b || b2;
+    }
+
+    private static boolean checkAndExecuteCommand() {
+        if ("exit".equals(command)) {
+            return true;
+        } else {
+            executeCommand();
+        }
+        return false;
+    }
+
+    private static void executeCommand() {
+        System.out.println("Executing command --> " + command);
+    }
+
+    private static void printInro() {
+        System.out.println("Welcome to OSQL!");
+        System.out.println("Press Help for commands");
+    }
+
+    static private boolean takeCommand() {
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        String isExit = myObj.nextLine();
+        command = isExit;
+        return "exit".equals(isExit);
+    }
+}
